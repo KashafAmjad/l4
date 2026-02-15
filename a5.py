fac04fc1ba11f7fc7d10373aadfe359c75c6f58a
KashafAmjad <shinningstarkshf@gmail.com>
2026-02-15 17:27:47 +0500

    Add files via upload

---
+++ a5.py
@@ -0,0 +1,9 @@
+#cheak age
+age=int(input("Enter your age:"))
+if age>10: 
+      if age>10 and age<=30:
+        print("you are allowed in sportsclub")
+      else:
+          print("you are not allowed")
+else:
+    print("you are too young")
