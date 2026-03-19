c536b2de9ab324d29c8a36a6807e16d2c4e540e9
KashafAmjad <shinningstarkshf@gmail.com>
2026-03-19 18:40:44 +0500

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,6 +1,9 @@
-def cube(n):
-    return n*n*n
-n=int(input("Enter any number:"))
-answer=cube(n)
-print("cube will be",answer)
-
+try:
+    age=int(input('Enter your age:'))
+    print("age:",age)
+    if age%2==0:
+        print("age number is even")
+    else:
+          print("age number is odd")   
+except:
+     print("Pls enter correct number or data")
